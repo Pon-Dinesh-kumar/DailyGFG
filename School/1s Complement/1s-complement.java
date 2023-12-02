55620ec99a7907d1e64f5b2be3636f6bb72cb381
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-12-02 21:04:14 +0530

    Added Solution - GfG to GitHub

---
+++ School/1s Complement/1s-complement.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String S = read.readLine();
+          
+
+            Solution ob = new Solution();
+            System.out.println(ob.onesComplement(S,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String onesComplement(String S, int N) {
+        char[] charArray = S.toCharArray();
+
+        for (int i = 0; i < N; i++) {
+            if (charArray[i] == '1') {
+                charArray[i] = '0';
+            } else {
+                charArray[i] = '1';
+            }
+        }
+
+        return new String(charArray);
+    }
+}
