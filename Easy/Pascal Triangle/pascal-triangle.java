0d0e608de3d0074cb7b992da5a74ef0955408039
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-12-22 23:37:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pascal Triangle/pascal-triangle.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            ArrayList<Long> ans = new Solution().nthRowOfPascalTriangle(n);
+            printAns(ans);
+        }
+    }
+    public static void printAns(ArrayList<Long> ans) {
+        for (Long x : ans) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        ArrayList<Long> result = new ArrayList<>();
+        long mod = 1000000007;
+
+        for (int i = 0; i < n; i++) {
+            result.add(1L);
+
+            for (int j = i - 1; j > 0; j--) {
+                result.set(j, (result.get(j) + result.get(j - 1)) % mod);
+            }
+        }
+
+        return result;
+    }
+}
