cb3a433641b5a5318a6449bec27537147f6d92a2
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-12-24 23:17:32 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.util.*;
+class PalindromicArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i = 0 ;i < n; i++)
+				a[i]=sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.palinArray(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG {
+    // Function to check if all elements in the array are palindrome.
+    public static int palinArray(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            if (!isPalindrome(a[i])) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    // Function to check if a number is palindrome.
+    static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+}
