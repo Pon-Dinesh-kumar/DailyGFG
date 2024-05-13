3fa21c6da92683220a73f4de2364336a771b7973
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2024-05-13 19:32:20 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Remove consonants from a string/remove-consonants-from-a-string.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.removeConsonants(s));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String removeConsonants(String s)
+    {
+        StringBuilder str = new StringBuilder();
+        
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
+        
+        for(int i =0; i< s.length(); i++){
+            if(set.contains(s.charAt(i))){
+                str.append(s.charAt(i));
+            }
+        }
+        if(str.length()==0){
+            return "No Vowel";
+        }
+        return str.toString();
+    }
+}
