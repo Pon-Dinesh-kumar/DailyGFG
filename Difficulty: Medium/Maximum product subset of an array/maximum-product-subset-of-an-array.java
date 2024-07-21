04abb35bdcc6b37cf27546d5f07c3cb422f80afc
Pon Dinesh Kumar M <m.pondineshkumar@gmail.com>
2024-07-21 22:41:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum product subset of an array/maximum-product-subset-of-an-array.java
+++ Difficulty: Medium/Maximum product subset of an array/maximum-product-subset-of-an-array.java
@@ -23,45 +23,30 @@ public class GFG {
 
 
 class Solution {
-    final static int mod = 1000000007;
-    
     public long findMaxProduct(List<Integer> arr) {
-        int n = arr.size();
-        if (n == 1) return arr.get(0);
+        if (arr.size() == 1) return arr.get(0);
         
-        // Sort the array
-        Collections.sort(arr);
-        
-        long max_product = 1;
-        int zero_count = 0;
-        int negative_count = 0;
-        int max_negative = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < n; i++) {
-            if (arr.get(i) == 0) {
-                zero_count++;
-                continue;
+        long prod = 1;
+        int zeroCount = 0, nonZeroCount = 0;
+        int maxNegative = Integer.MIN_VALUE;
+        final int MOD = 1000000007;
+
+        for (int num : arr) {
+            if (num != 0) {
+                prod = (prod * num) % MOD;
+                nonZeroCount = 1;
+            } else {
+                zeroCount = 1;
             }
-            
-            if (arr.get(i) < 0) {
-                negative_count++;
-                max_negative = Math.max(max_negative, arr.get(i));
+            if (num < 0 && num > maxNegative) {
+                maxNegative = num;
             }
-            
-            max_product = (max_product * arr.get(i)) % mod;
         }
+
+        if (nonZeroCount != 1) return 0;
+        if (arr.size() == 2 && zeroCount == 1 && prod < 0) return 0;
+        if (prod < 0) prod /= maxNegative;
         
-        // If all numbers were zero
-        if (zero_count == n) return 0;
-        
-        // If count of negative numbers is odd
-        if (negative_count % 2 == 1) {
-            // If there were more than one negative number and no positive numbers, 
-            // remove the largest negative number from the product
-            if (negative_count != 1 || zero_count > 0 || zero_count + negative_count != n)
-                max_product = (max_product / max_negative) % mod;
-        }
-        
-        return max_product;
+        return prod;
     }
 }
